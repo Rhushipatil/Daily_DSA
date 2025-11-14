afdd2eaa17067f71b95c816fc96ef138f13ead89
Rhushikesh Patil <123874020+Rhushipatil@users.noreply.github.com>
2025-11-14 13:18:14 +0530

    Add files via upload

---
+++ python dsa/day_1.py
@@ -0,0 +1,48 @@
+#a=input("enter your no:")
+#print(a)
+
+# a=2
+# b=5
+# print(a+b)
+
+# a="ram"
+# b="sham"
+# print(a+b)
+
+#### iff else
+# a=int(input("enter your marks:"))
+# if a>=90:
+#     print("grade a")
+# elif a>=70:
+#     print("grade b")
+# elif a>=50:
+#     print("grade c")
+# elif a>35:
+#     print("grade d")
+# else:
+#     print("fail")               
+
+
+
+
+### switch case (day code)
+
+# day=int(input("enter the no of day"))
+
+# if day==1:
+#     print("monday")
+# elif day==2:
+#     print("tuesday")
+# elif day==3:
+#     print("wed")
+# elif day==4:
+#     print("thr")
+# elif day==5:
+#     print("fri")
+# elif day==6:
+#     print("sat")
+# elif day==7:
+#     print("sun")                    
+# elif day>8:
+#     print("invalid numbre of week")    
+
