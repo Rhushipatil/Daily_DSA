667833e30f798eef094ce23e7d287258f2f3780a
Rhushikesh Patil <123874020+Rhushipatil@users.noreply.github.com>
2025-11-21 23:31:50 +0530

    Update day_2(pattern).py

--- python dsa/day_2(pattern).py
+++ python dsa/day_2(pattern).py
@@ -4,4 +4,42 @@
 for i in range(1,6):
     for j in range(1,6):
         print("*", end="" )
+
     print()    
+
+##2
+# n=int(input("enter no :"))    
+
+# for i in range(1,n+1):
+#     for j in range(i):
+#         print("*" ,end="")
+#     print()    
+   
+##3 
+
+# for i in range(2,6):
+   
+#     for j in range(1,i+1):
+#         print(j, end="")
+#     print()    
+
+##4
+
+# for i in range(1,6):
+#     for j in range(i):
+#         print(i, end="")
+#     print()    
+
+## 5
+
+# for i in range(5,0,-1):
+#     for j in range(1,i+1):
+#         print(j , end="")
+#     print()    
+
+
+##6
+# for i in range(5,0,-1):
+#     for j in range(i):
+#         print("*" , end="")
+#     print()    
