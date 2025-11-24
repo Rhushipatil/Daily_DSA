d00c1c507c0c7da91a3c599e23f24a1bb584d80c
Rhushikesh Patil <123874020+Rhushipatil@users.noreply.github.com>
2025-11-24 11:00:28 +0530

    Update day_2(pattern).py

--- python dsa/day_2(pattern).py
+++ python dsa/day_2(pattern).py
@@ -43,3 +43,53 @@ for i in range(1,6):
 #     for j in range(i):
 #         print("*" , end="")
 #     print()    
+
+## 7
+# n=5
+# for i in range(1,n+1):
+#     space="   " *(n-i)
+#     star= " * " *(2* i-1)
+#     print(space + star)
+
+
+## 8
+
+# n=5
+# for i in range(n,0,-1):
+#     space=" " *(n-i)
+#     star="*" *(2*i -1)
+#     print(space + star)
+    
+
+##9
+
+# n=5
+# for i in range(1, n+1):
+#     space=" " * (n-i)
+#     star="*" * (2*i-1)
+#     print(space + star)    
+    
+# for j in range(n-1,0,-1):
+#     space=" " * (n-j)
+#     star="*" * (2*j-1)
+#     print(space + star)    
+    
+    #10
+    
+# n=5
+
+# for i in range(1,n+1):
+#     print("*" * i)    
+# for j in range(n,0,-1):
+#     print("*" * j)     
+
+
+## 11
+
+# n=5
+# for i in range(1,n+1):
+#     val=i  %2
+#     for _ in range(i):
+#         print(val, end="")
+#         val= 1- val
+#     print()    
