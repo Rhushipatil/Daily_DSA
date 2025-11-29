d3b311d5444b2275b56f1cd666792519cb19a888
Rhushikesh Patil <123874020+Rhushipatil@users.noreply.github.com>
2025-11-29 23:18:22 +0530

    Update Questions_recursions(2).py

--- python dsa/Questions_recursions(2).py
+++ python dsa/Questions_recursions(2).py
@@ -9,4 +9,41 @@ def prt_name(name,n):
     print(name)
     prt_name(name ,n-1)
     
+
 prt_name("rhushi", 3)    
+
+#Print 1 to N using Recursion
+
+
+
+# def prt(n,num=1):
+#     if num>n:
+#         return
+#     print(num)
+#     prt(n,num+1)
+    
+# prt(5)    
+    
+#or
+
+# def print_1_to_n(n):
+#     if n==0:     # base condition
+#         return
+#     print_1_to_n(n-1) 
+#     print(n)
+    
+
+# print_1_to_n(5)
+
+
+
+
+#Print N to 1 using Recursion
+def prt(n):
+    
+    if n<1:
+        return
+    print(n , end=" ")
+    prt(n-1)
+        
+prt(6)
