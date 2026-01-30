4152acc84e47b5834899cd935cb517334db8650c
Rhushikesh Patil <rhushikesh2005@gmail.com>
2026-01-30 23:41:36 +0530

    Add files via upload

---
+++ java dsa/javabasic.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+public class javabasic {
+    public static void main(String[] args) {
+        //System.out.print("Hello World!\n");
+       // System.out.print("hello world");
+       /*int a =10;
+       int b=20;
+       System.out.println(a+" "+b);  // java doesnot support sout(a,b);
+       System.out.println(b);
+        
+       a=b;
+       System.out.println(a);*/
+       /*Scanner sc=new Scanner(System.in);
+        System.out.print("Enter what you need: ");
+         String input = sc.next();
+         System.out.println(input);
+  
+        sc.nextLine();
+        System.out.println("enter name:");
+        String name=sc.nextLine();
+        System.out.println(name);*/
+
+
+        // sum of a and b
+       /*  Scanner sc=new Scanner(System.in);
+        System.out.println("enter the value of a and b");
+        int a=sc.nextInt();
+        int b= sc.nextInt();
+        int c= a+b;
+        System.out.println("a+b is" + " " + c);*/
+
+        // area of circle
+        /*Scanner sc=new Scanner(System.in);
+        System.out.println("enter the radius");
+        int radius=sc.nextInt();
+        Float area= 3.14f * radius * radius;
+        System.out.println("area is :" + area);*/
+
+        //type casring
+        float a = 12.55f;
+        int b = (int) a;   
+        System.out.println(b);
+
+    }
+}
